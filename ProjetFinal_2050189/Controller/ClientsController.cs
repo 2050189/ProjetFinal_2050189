cb529615f41bfc9261bb89d579681c39bc71e232
sarah <2050189@cegepmontpetit.ca>
2024-05-02 11:57:17 -0400

    PROGRES Controller Chiffrement

---
+++ ProjetFinal_2050189/Controller/ClientsController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProjetFinal_2050189.Data;
+using ProjetFinal_2050189.Models;
+
+namespace ProjetFinal_2050189.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly ProjetFinal2050189Context _context;
+
+        public ClientsController(ProjetFinal2050189Context context)
+        {
+            _context = context;
+        }
+
+        public async Task Dechiffrer(int id)
+        {
+            string query = "EXEC VENTE.USP_RecupererNumClient @ClientID";
+
+            SqlParameter parametres = new SqlParameter { ParameterName = "@ClientID", Value = id };
+
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync(query, parametres);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "");
+            }
+
+            await _context.Clients.FindAsync(id);
+        }
+
+        // GET: Clients
+        public async Task<IActionResult> Index()
+        {
+              return _context.Clients != null ? 
+                          View(await _context.Clients.ToListAsync()) :
+                          Problem("Entity set 'ProjetFinal2050189Context.Clients'  is null.");
+        }
+
+        // GET: Clients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+            //ClientDechiffre clientdechiffre = (await _context.ClientDechiffres.FromSqlRaw("query",).ToListAsync()).FirstOrDefaultAsync();
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+
+            
+
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientId,Prenom,NumeroTel")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(client);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClientId,Prenom,NumeroTel")] Client client)
+        {
+            if (id != client.ClientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.ClientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Clients == null)
+            {
+                return Problem("Entity set 'ProjetFinal2050189Context.Clients'  is null.");
+            }
+            var client = await _context.Clients.FindAsync(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientExists(int id)
+        {
+          return (_context.Clients?.Any(e => e.ClientId == id)).GetValueOrDefault();
+        }
+    }
+}
